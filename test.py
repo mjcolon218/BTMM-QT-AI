586b05ee57b39b3f962f2d228c22cb75937b683b
Maurice <mjcolon218@gmail.com>
2025-10-02 13:47:39 -0400

    new updates with improved scoring system

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 index = faiss.read_index("kb/vectors/trading.faiss")
 metas = [json.loads(l) for l in open("kb/vectors/meta.jsonl")]
 
-query = "tell me how the BTMM strategy works with entries and exits and the Quarters method and what should i look for in confirmation and confluence and how does these two strats align up so I can make high probability trades"
+query = "Does the stop hunts happen at the quarters "
 vec = model.encode([query], normalize_embeddings=True).astype("float32")
 D, I = index.search(vec, 3)
 for i in I[0]:
